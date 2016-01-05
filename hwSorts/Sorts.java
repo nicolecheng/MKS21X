0f3738a508a30f0e0f593e5ba6ee527c0e830aae
nicolecheng <ncheng@stuy.edu>
2016-01-05 16:06:59 -0500

    bubble sort done

--- hwSorts/Sorts.java
+++ hwSorts/Sorts.java
@@ -16,7 +16,7 @@ public class Sorts{
 	System.out.println(ret + "]");
     }
     
-    public static void insertion(int[]data){
+    public static void insertionSort(int[]data){
 	//your code here to make data re-order its elements
 	//from least to greatest just like we did in class
 	//same algorithm as the way you created your OrderedSuperArray
@@ -41,13 +41,15 @@ public class Sorts{
 			data[i] = data[i-1];
 		    }
 		    data[insert] = hold;
-		    index++;		
+		    index++;	
+		    printArray(data);	
 		}
 	    }
 	}
     }
 
-    public static void selection(int[]data){
+
+    public static void selectionSort(int[]data){
 	if (data.length > 1){
             int index = 0;
 	    while (index < data.length){
@@ -62,41 +64,71 @@ public class Sorts{
 		data[minInd] = data[index];
 		data[index] = min;
 		index++;
+		printArray(data);	
 	    }
-
 	}
-     }
-
-      public static void main(String[]args){
-	  int[] arrayName = { 8, 6, 7, 5, 3, 0, 9};
-	  Sorts.printArray( arrayName);
-	  Sorts.selection( arrayName);
-	  Sorts.printArray( arrayName);
-	
-	  int[] e = {};
-	  Sorts.printArray( e);
-	  Sorts.selection( e);
-	  Sorts.printArray( e);
-	
-	  int[] ae = { 8, 8, 8, 8, 8};
-	  Sorts.printArray( ae);
-	  Sorts.selection( ae);
-	  Sorts.printArray( ae);
-
-	  int[] a = { 9, -9, 6, 3, 17};
-	  Sorts.printArray( a);
-	  Sorts.selection( a);
-	  Sorts.printArray( a);
-
-	  int[] ar = { 0};
-	  Sorts.printArray( ar);
-	  Sorts.selection( ar);
-	  Sorts.printArray( ar);
-
-	  int[] array = { 15, 14, 13, 12, 11, 10};
-	  Sorts.printArray( array);
-	  Sorts.selection( array);
-	  Sorts.printArray( array);
-	  }
-
     }
+
+    public static void bubbleSort(int[]data){
+	int sorted = data.length; // from which index is it sorted?
+	int hold = 0;
+	while (sorted > 0){
+	    for (int i = 0; i < sorted-1; i++){
+		if (data[i] > data[i+1]){
+		    hold = data[i+1];
+		    data[i+1] = data[i];
+		    data[i] = hold;
+		}
+	    }
+	    sorted --;
+	    printArray(data);
+	}
+    }
+
+    public static void main(String[]args){
+	System.out.println("Selection Sort:");
+	int[] arrayName = { 8, 6, 7, 5, 3, 0, 9};
+	Sorts.printArray( arrayName);
+	Sorts.selectionSort( arrayName);
+	Sorts.printArray( arrayName);
+	System.out.println();
+	System.out.println("Insertion Sort:");
+	int[] arrayN = { 8, 6, 7, 5, 3, 0, 9};
+	Sorts.printArray( arrayN);
+	Sorts.insertionSort( arrayN);
+	Sorts.printArray( arrayN);
+	System.out.println();
+	System.out.println("Bubble Sort:");
+	int[] arryN = { 8, 6, 7, 5, 3, 0, 9};
+	Sorts.printArray( arryN);
+	Sorts.bubbleSort( arryN);
+	Sorts.printArray( arryN);
+	/*	
+		int[] e = {};
+		Sorts.printArray( e);
+		Sorts.selection( e);
+		Sorts.printArray( e);
+	
+		int[] ae = { 8, 8, 8, 8, 8};
+		Sorts.printArray( ae);
+		Sorts.selection( ae);
+		Sorts.printArray( ae);
+
+		int[] a = { 9, -9, 6, 3, 17};
+		Sorts.printArray( a);
+		Sorts.selection( a);
+		Sorts.printArray( a);
+
+		int[] ar = { 0};
+		Sorts.printArray( ar);
+		Sorts.selection( ar);
+		Sorts.printArray( ar);
+
+		int[] array = { 15, 14, 13, 12, 11, 10};
+		Sorts.printArray( array);
+		Sorts.selection( array);
+		Sorts.printArray( array);
+	*/
+    }
+
+}
